4b9647fd10d284e40cc7b8f487bd526641dfd833
Wesley van Balen <wvanbalen@gmail.com>
2020-12-28 20:50:19 +0100

    Added new hero and about section

---
+++ Alstar/js/gsap_anim.js
@@ -0,0 +1,52 @@
+gsap.registerPlugin(ScrollTrigger);
+
+var tl = gsap.timeline();
+
+tl.from('.content', {
+  y: '-30%',
+  opacity: 0,
+  duration: 2,
+  ease: Power4.easeOut
+})
+tl.from('.stagger1', {
+  y: '-50',
+  opacity: 0,
+  stagger: .3,
+  ease: Power4.easeOut,
+  duration: 2
+}, "-=1.5")
+tl.from('.hero-design', {
+  y: '50',
+  opacity: 0,
+  ease: Power4.easeOut,
+  duration: 1
+}, "-=2")
+
+gsap.from(".square-anim", {
+  stagger: .2,
+  scale: 0.1,
+  duration: 1,
+  ease: Back.easeOut.config(0.5)
+})
+
+gsap.from(".transition2", {
+  scrollTrigger: {
+    trigger: '.transition2',
+    start: "top bottom"
+  },
+  y: 50,
+  opacity: 0,
+  duration: 1.2,
+  stagger: .3
+})
+
+gsap.from(".transition3", {
+  scrollTrigger: {
+    trigger: '.transition3',
+    start: "top center"
+  },
+  y: 50,
+  opacity: 0,
+  duration: 1.2,
+  stagger: .6
+})
